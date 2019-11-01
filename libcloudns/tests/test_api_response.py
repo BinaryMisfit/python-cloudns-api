826d929455f220474e806616ae11887a6a0ecd4b
Willie Roberts <diagoza@me.com>
2019-11-01 16:07:38 +0000

    Added additional test for ap_response

--- libcloudns/tests/test_api_response.py
+++ libcloudns/tests/test_api_response.py
@@ -4,6 +4,18 @@ from libcloudns import Response
 
 
 class TestResponse(TestCase):
+    def test_has_repr(self):
+        response = Response()
+        expected = "<Response version=0.0.1>"
+        actual = repr(response)
+        self.assertEqual(actual, expected)
+
+    def test_has_str(self):
+        response = Response()
+        expected = "0.0.1"
+        actual = str(response)
+        self.assertEqual(actual, expected)
+
     def test_has_version(self):
         response = Response()
         self.assertIsInstance(response.version, str)
