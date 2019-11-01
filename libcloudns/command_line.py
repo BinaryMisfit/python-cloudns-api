5e215494dbb5c764d174cebb02c5b128a67326c1
Willie Roberts <diagoza@me.com>
2019-11-01 19:46:06 +0000

    Fixed linter issues for command_line.py

--- libcloudns/command_line.py
+++ libcloudns/command_line.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
+"""Command line functionality for ClouDNS APi
+"""
 from .api_response import Response
 
 
 def build_version():
+    """Build the output for the main method
+    """
     response = Response()
     version = "Response version: {0}".format(response.version)
     return version
 
+
 def main():
+    """Print the version information for ClouDNS API
+    """
     print(build_version())
