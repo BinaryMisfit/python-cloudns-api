2613b835c874a0610260cbf1cf3e28654c99c625
Willie Roberts <diagoza@me.com>
2019-11-01 16:16:14 +0000

    Added additional tests for command_line

--- libcloudns/tests/test_command_line.py
+++ libcloudns/tests/test_command_line.py
@@ -6,3 +6,12 @@ from libcloudns import command_line
 class TestCommandLine(TestCase):
     def test_has_main(self):
         self.assertIsInstance(command_line.build_version(), str)
+
+    def test_has_output(self):
+        from io import StringIO
+        from unittest.mock import patch
+
+        expected = command_line.build_version()
+        with patch('sys.stdout', new=StringIO()) as output:
+            command_line.main()
+            self.assertEqual(output.getvalue().strip(), expected)
